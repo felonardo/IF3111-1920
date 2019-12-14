aa2d064cf8c12af8dded207f85872a101f50df58
andika168 <andikaharispratama03@gmail.com>
2019-12-14 09:21:24 +0700

    Membuat view Lihat

---
+++ resources/views/Lihat.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="lihat.css">
+    <title>Lihat Selengkapnya</title>
+</head>
+
+<body>
+    <header>
+
+        <div id="header">
+            <h1>SIMPLE LAPOR!</h1>
+        </div>
+
+
+    </header>
+    <div class="isi">
+        <p>Detail Laporan/Komentar</p>
+        <hr>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam, maxime dolore ratione
+            voluptatibus
+            dignissimos amet pariatur eaque laboriosam minus fuga, expedita sint laborum ipsa inventore
+            obcaecati
+            sunt illo blanditiis! Iste porro dolores dicta laboriosam. Officiis adipisci harum officia nostrum
+            necessitatibus eos neque assumenda non et repellendus sunt, blanditiis nobis libero, voluptatibus
+            cumque
+            nemo possimus laudantium alias nam repudiandae veniam? Eos quas minus nulla pariatur nisi commodi,
+            sapiente esse explicabo inventore, nesciunt incidunt voluptatibus porro officiis illum dolorem
+            voluptatum molestiae aut laudantium a nihil accusamus. Dolor quidem ducimus, nihil non consequuntur
+            tempore! Veritatis, recusandae tempora accusantium tenetur a reiciendis! Voluptas, veritatis.</p>
+
+        <p>Lampiran:</p>
+        <img src="kotak.png" height="200" width="200" alt="ini gambar kotak">
+        <br>
+        <div class="detail">
+
+            <p>Waktu</p>
+            <p>Aspek</p>
+            <p class="tombol">Hapus Laporan/Komentar &#9746;</p>
+        </div>
+        <hr>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
